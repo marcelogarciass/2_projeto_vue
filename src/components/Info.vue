77d80ee77eb9680db21bc4e4af5ca0875648445f
marcelogarciass <marcelogarciass@hotmail.com>
2023-04-16 11:07:36 -0300

    atributos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -11,16 +11,21 @@
             <li>HTML</li>
         </ul>
         <p v-show="mostrar_email" >Mande uma mensagem para: {{ email }} </p>
+        <p>Para acessar meu portifolio basta clicar no link <a v-bind:href=" meu_link " target="Blank" >clique aqui</a> </p>
+        <Picture/>
     </div>
 </template>
 <script>
+import Picture from './Picture.vue'
 export default {
+  components: { Picture },
     nome: 'Info',
     data() {
         return{
             esta_trabalhando: true,
             mostrar_email: false,
-            email: 'marcelogarciass@hotmail.com'
+            email: 'marcelogarciass@hotmail.com',
+            meu_link: 'https://google.com'
         }
     }
 }
---
+++ src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p><img :src="avatar" :alt="descricao"></p>
+    </div>
+</template>
+<script>
+    export default {
+    name: 'Picture',
+    data() {
+        return {
+            avatar: '/img/avatar.jpg',
+            descricao: 'Marcelo Garcia'
+        }
+    }
+}
+</script>
