14bccca5f15a3940371c45383c820b44b0644dad
marcelogarciass <marcelogarciass@hotmail.com>
2023-04-17 21:41:40 -0300

    css global and scoped

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <Header/>
   <primeiro-componente/>
   <life-cycle/>
   <pessoa/>
   <Form/>
+  
 </template>
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 import LifeCycle from './components/LifeCycle.vue';
 import Pessoa from './components/Pessoa.vue';
 import Form from './components/Form.vue';
+import Header from './components/Header.vue';
 export default {
   name: 'App',
   components: {
@@ -16,6 +19,18 @@ export default {
     LifeCycle,
     Pessoa,
     Form,
+    Header,
   }
 }
 </script>
+
+<style>
+body {
+  background-color: darkcyan;
+  color: aliceblue;
+}
+a {
+  color: brown;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projetos</a></li>
+        <li><a href="#">Contatos</a></li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        display: flex;
+    }
+
+    li {
+        margin-right: 10px;
+    }
+
+    a {
+        color: azure;
+        text-decoration: none;
+        transition: .5s;
+    }
+
+    a:hover {
+        color: blue;
+    }
+</style>
